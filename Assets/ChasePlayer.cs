ce0365ef9fd7fcf75c6519a84de2db2407ce0555
Zihan Yang <zihan_yang_1996@outlook.com>
2024-04-22 16:46:33 -0400

    Try AI Navigation in sendbox, add to enemy to chase the player

---
+++ Assets/ChasePlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChasePlayer : MonoBehaviour
+{
+    [SerializeField] Transform player;
+
+    NavMeshAgent agent;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.position);
+    }
+}
