348db232541460e448b62ddc8bef96789228d5ca
Zihan Yang <zihan_yang_1996@outlook.com>
2024-04-23 15:18:10 -0400

    Update the chasing logic, enemy won't stop right away after losing the target, instead it will keep moving towards the last known position of the target for a few seconds

--- Assets/ChasePlayer.cs
+++ Assets/ChasePlayer.cs
@@ -11,8 +11,13 @@ public class ChasePlayer : MonoBehaviour
 
     [Tooltip("The square of the distance within which the enemy will chase the player")]
     public float Radius = 10f;
+    [SerializeField] float chaseTimeAfterLosesTarget = 5f;
     float squareRadius;
     float squareDistance;
+
+    bool isProvoked = false;
+    Coroutine keepChasingCoroutine;
+
 
     NavMeshAgent agent;
 
@@ -25,17 +30,83 @@ public class ChasePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isProvoked)
+        {
+            EngaeTarget();
+        }
+        else
+        {
+            CalculateDistance();
+            if (squareDistance <= squareRadius)
+            {
+                isProvoked = true;
+            }
+        }
+
+
+        // if (squareDistance <= squareRadius)
+        // {
+        //     
+        //     agent.SetDestination(player.position);
+        // }
+        // else
+        // {
+        //     agent.isStopped = true;
+        //     // or agent.SetDestination(transform.position);
+        // }
+    }
+
+    void EngaeTarget()
+    {
         CalculateDistance();
         if (squareDistance <= squareRadius)
         {
+            // Check if the coroutine is null, if not stop the coroutine
+            // Note that the coroutine stopped either by itself or by the StopCoroutine method won't be null
+            if (keepChasingCoroutine != null)
+            {
+                Debug.Log("Stopping the coroutine");
+                StopCoroutine(keepChasingCoroutine);
+                keepChasingCoroutine = null;  // Reset the coroutine to null
+            }
             agent.isStopped = false;
             agent.SetDestination(player.position);
         }
         else
         {
-            agent.isStopped = true;
-            // or agent.SetDestination(transform.position);
+            if (keepChasingCoroutine == null)
+            {
+                // get the last known position of the player
+                Vector3 lastKnownPosition = player.position;
+                keepChasingCoroutine = StartCoroutine(KeepChasing(lastKnownPosition, chaseTimeAfterLosesTarget));
+            }
         }
+    }
+
+    // Keep moving towards the last known position of the player
+    IEnumerator KeepChasing(Vector3 lastKnownPosition, float chaseTimeAfterLosesTarget)
+    {
+        // Simple but less control over the behavior
+        // Debug.Log("Target lost, moving to the last known position");
+        // agent.SetDestination(lastKnownPosition);
+        // // float timeElapsed = 0;
+        // yield return new WaitForSeconds(chaseTimeAfterLosesTarget);
+        // Debug.Log("Stopped chasing");
+        // agent.isStopped = true;
+
+        // More control over the behavior
+        Debug.Log("Target lost, moving to the last known position");
+        agent.SetDestination(lastKnownPosition);
+        float timeElapsed = 0;
+        while (timeElapsed < chaseTimeAfterLosesTarget)
+        {
+            timeElapsed += Time.deltaTime;
+            // Can be used to add some behavior while chasing
+            yield return new WaitForEndOfFrame();
+            
+        }
+        Debug.Log("Stopped chasing");
+        agent.isStopped = true;
     }
 
     void CalculateDistance()
@@ -45,12 +116,8 @@ public class ChasePlayer : MonoBehaviour
 
     void OnDrawGizmosSelected()
     {
-        if (!Application.isPlaying)
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(transform.position, Radius);
-        }
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, Radius);
     }
-
-
 }
