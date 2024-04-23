ad5699ea02c74087fc5bf08184779e7ab5409d6f
Zihan Yang <zihan_yang_1996@outlook.com>
2024-04-23 13:06:33 -0400

    Make enemy only chase the player when player is within certain distance

--- Assets/ChasePlayer.cs
+++ Assets/ChasePlayer.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.EditorTools;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,16 +9,37 @@ public class ChasePlayer : MonoBehaviour
 {
     [SerializeField] Transform player;
 
+    [Tooltip("The square of the distance within which the enemy will chase the player")]
+    [SerializeField] float radius = 10f;
+    float squareRadius;    
+    float squareDistance;
+
     NavMeshAgent agent;
 
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        squareRadius = radius * radius;
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(player.position);
+        CalculateDistance();
+        if (squareDistance <= squareRadius)
+        {
+            agent.isStopped = false;
+            agent.SetDestination(player.position);
+        }
+        else
+        {
+            agent.isStopped = true;
+            // or agent.SetDestination(transform.position);
+        }
+    }
+
+    void CalculateDistance()
+    {
+        squareDistance = (player.position - transform.position).sqrMagnitude;
     }
 }
